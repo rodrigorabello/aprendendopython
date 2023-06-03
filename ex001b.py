9750da5d320081d0c7f016bb1fb7da65d77b8be6
rodrigorabello <rodrigorabello@outlook.com>
2023-06-03 17:22:45 -0300

    Meu primeiro testes

---
+++ ex001b.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite o valor do primeiro número: '))
+n2 = int(input('Digite o valor do segundo número'))
+s = n1 + n2
+print('O valor da soma de {} e {} é {}'.format(n1,n2 , s))
+
