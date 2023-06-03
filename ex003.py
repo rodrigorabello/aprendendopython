3a94d43f89d35bef914d917d0d679716dad165a2
rodrigorabello <rodrigorabello@outlook.com>
2023-06-03 19:11:31 -0300

    Meu primeiro testes

---
+++ ex003.py
@@ -0,0 +1,5 @@
+s = int(input ('Digite um número e eu vou te mostrar o seu sucessor e seu antecessor ... :'))
+n1 = s + 1
+n2 = s - 1
+print('o antecessor de {} é {} e o seu sucessor é {}'.format(s, n2, n1))
+
---
+++ ex004.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite a primeira nota do aluno: '))
+n2 = float(input('Digite a segunda nota do aluno: '))
+m = (n1 + n2) / 2
+print('A média final do aluno é {:.2f}'.format(m))
